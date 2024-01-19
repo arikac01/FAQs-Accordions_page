6a5d309490101d3ea12cfbe72a38b05c006dc162
Chandana A N <chandanaroyal29@gmail.com>
2024-01-19 03:15:58 -0500

    Link updates in JS file

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const ToggleIcon = (icon, content)=>{
-    //let icon = document.querySelector(".plusicon");
-    //let content = document.querySelector('.section-content');
-
-   if (icon.src.includes("/faq-accordion-main/assets/images/icon-plus.svg")){
-    icon.src = "/faq-accordion-main/assets/images/icon-minus.svg";
+   if (icon.src.includes("/assets/images/icon-plus.svg")){
+    icon.src = "/assets/images/icon-minus.svg";
     content.style.display = "block";
    }
    else{
-    icon.src = "/faq-accordion-main/assets/images/icon-plus.svg";
+    icon.src = "/assets/images/icon-plus.svg";
     content.style.display = "none";
    }
 }
